ad58b030f06a59c54102385c13b460a86c4b580e
Alex Werner <akw.werner@icloud.com>
2025-05-15 21:03:34 -0500

    Login Screen started

---
+++ components/CustomButton.js
@@ -0,0 +1,22 @@
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+export default function CustomButton(title) {
+    return(
+        <TouchableOpacity style={styles.button} onPress={ () => {}}>
+            <Text style={[styles.text]}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#6C5CE7',
+        borderRadius: 20,
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontFamily: 'System',
+    },
+        })
---
+++ screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import CustomButton from '../components/CustomButton';
+
+export default function LoginScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Party Pass</Text>
+            <CustomButton title='Login'/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
