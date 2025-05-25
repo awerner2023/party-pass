95faeb75323eda298f5187633845022d3c29688b
Alex Werner <akw.werner@icloud.com>
2025-05-24 21:27:24 -0600

    Installed and added tab navigation to access different screens.

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {StyleSheet, Text, View, Button, SafeAreaView} from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-      <HomeScreen/>
+  const TabNavigator = createBottomTabNavigator();
+    return (
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <TabNavigator.Navigator>
+                <TabNavigator.Screen name = 'Home' component={HomeScreen} options={{headerShown: false}} />
+                <TabNavigator.Screen name = 'Login' component={LoginScreen} options={{headerShown: false}} />
+            </TabNavigator.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
   );
 }
 
