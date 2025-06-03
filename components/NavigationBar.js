d1d8e51f9a1f43a8da9eb5d30d05f3d64367084e
Alex Werner <akw.werner@icloud.com>
2025-06-02 21:00:14 -0500

    started figuring out icons

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -4,13 +4,16 @@ import LoginScreen from "../screens/LoginScreen";
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import ProfileScreen from "../screens/ProfileScreen";
+import {Ionicons} from '@expo/vector-icons'
 
 export default function NavigationBar() {
     const TabNavigator = createBottomTabNavigator();
     return (<NavigationContainer>
             <TabNavigator.Navigator
                 screenOptions={containerStyle.tabNavigator}>
-                <TabNavigator.Screen name='Home' component={HomeScreen} options={{headerShown: false}}/>
+                <TabNavigator.Screen name='Home' component={HomeScreen} options={{headerShown: false} }
+                    <Ionicons name={'home'} size = '24' color={'black'}/>
+                />
                 <TabNavigator.Screen name='Login' component={LoginScreen} options={{headerShown: false}}/>
                 <TabNavigator.Screen name='Profile' component={ProfileScreen} options={{headerShown: false}}/>
             </TabNavigator.Navigator>
