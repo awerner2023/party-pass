baeb97301c5a01ca459b7a18ad758fa5b4c833e4
Alex Werner <akw.werner@icloud.com>
2025-06-26 20:50:12 -0500

    Added text inputs to CreateAccountScreen.js

--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -1,10 +1,18 @@
-import {View, Text, TouchableOpacity} from "react-native";
+import {View, Text, TouchableOpacity, TextInput} from "react-native";
 import {containerStyle} from "../components/styles";
 import ScreenWrapper from "../components/ScreenWrapper";
+import Spacer from "../components/Spacer";
 
 export default function ProfileScreen() {
     return (
         <ScreenWrapper>
+            <TextInput style={containerStyle.textBoxContainer}
+                       placeholder={'Username'}
+            ></TextInput>
+            <Spacer height={spacerSize}/>
+            <TextInput style={containerStyle.textBoxContainer}
+                       placeholder={'Password'}
+            ></TextInput>
         </ScreenWrapper>
     )
 }
