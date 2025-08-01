2765f409078e30d63a9516689ce0a308fc2e0fd1
Alex Werner <akw.werner@icloud.com>
2025-07-31 21:16:39 -0500

    Added firebase config and registered web app in firebase.com

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -1,3 +1,16 @@
 
 import {initializeApp} from './firebase'
 import {getAuth} from '../firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCMN6IDgGjC95TqS2Y2Aczfom46lsB_lGg",
+    authDomain: "party-pass-467119.firebaseapp.com",
+    projectId: "party-pass-467119",
+    storageBucket: "party-pass-467119.firebasestorage.app",
+    messagingSenderId: "852980020033",
+    appId: "1:852980020033:web:ec27bb54f554d57e5a27c0",
+    measurementId: "G-097JCJQ8TC"
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+export const auth = getAuth(firebaseApp)
