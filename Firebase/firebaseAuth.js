45d7a4d0c5b5b051c4539e95a606a163a238b032
Alex Werner <akw.werner@icloud.com>
2025-08-01 21:10:59 -0500

    Added firebaseAuth.js and created login and new user functions.

---
+++ Firebase/firebaseAuth.js
@@ -0,0 +1,20 @@
+import {auth} from 'firebase.js'
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth'
+
+export const loginUser = async (email,password) => {
+    try{
+        const userInfo = await signInWithEmailAndPassword(auth,email,password)
+        return {user: userInfo.user}
+    } catch(error){
+        return {error: error.message}
+    }
+};
+
+export const createUser = async (email,password) => {
+    try {
+        const newUserInfo = await createUserWithEmailAndPassword(auth, email, password)
+        return {user: newUserInfo}
+    } catch(error){
+        return {error: error.message}
+    }
+}
