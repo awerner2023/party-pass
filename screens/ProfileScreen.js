c18e5f7a39e6ef6fa989d795bb7fd2655a8fc0bf
Alex Werner <akw.werner@icloud.com>
2025-08-22 17:22:00 -0500

    Reformatted file.

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -10,16 +10,16 @@ export default function ProfileScreen() {
     const navigation = useNavigation();
 
     return (<ScreenWrapper>
-            <View style={[containerStyle.header, {position: 'relative'}]}>
-                <Text style={textStyles.headerText}>My Profile</Text>
-            </View>
-            <View style={containerStyle.container}>
-            </View>
-            <View style={[containerStyle.container, {position: 'absolute', bottom: RFValue(100)}]}>
-                <CustomButton style={{width: '60%'}} title={'Event Mode'}></CustomButton>
-            </View>
-            <View style={[containerStyle.container, {position: 'absolute', bottom: RFValue(40)}]}>
-                <CustomButton style={{width: '60%'}} title={'Log Out'} onPress={signOutUser}></CustomButton>
-            </View>
-        </ScreenWrapper>)
+        <View style={[containerStyle.header, {position: 'relative'}]}>
+            <Text style={textStyles.headerText}>My Profile</Text>
+        </View>
+        <View style={containerStyle.container}>
+        </View>
+        <View style={[containerStyle.container, {position: 'absolute', bottom: RFValue(100)}]}>
+            <CustomButton style={{width: '60%'}} title={'Event Mode'}></CustomButton>
+        </View>
+        <View style={[containerStyle.container, {position: 'absolute', bottom: RFValue(40)}]}>
+            <CustomButton style={{width: '60%'}} title={'Log Out'} onPress={signOutUser}></CustomButton>
+        </View>
+    </ScreenWrapper>)
 }
